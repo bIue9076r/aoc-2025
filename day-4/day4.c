35a0e1ae2d933cd672a136e55d4760687b8abe9c
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-12-05 16:51:56 -0600

    day 4 change

--- day-4/day4.c
+++ day-4/day4.c
@@ -4,85 +4,67 @@
 
 static int Rolls = 0;
 
-typedef struct tile_t_s {
-	int isRoll;
-	struct tile_t_s* up;
-	struct tile_t_s* down;
-	struct tile_t_s* left;
-	struct tile_t_s* right;
-	struct tile_t_s* up_left;
-	struct tile_t_s* down_left;
-	struct tile_t_s* up_right;
-	struct tile_t_s* down_right;
-} tile_t;
-
-tile_t* newTile(int roll){
-	tile_t* r = malloc(sizeof(tile_t));
-	r->isRoll = roll;
-	r->up = NULL;
-	r->down = NULL;
-	r->left = NULL;
-	r->right = NULL;
-	r->up_left = NULL;
-	r->down_left = NULL;
-	r->up_right = NULL;
-	r->down_right = NULL;
-
-	return r;
-}
-
-tile_t* ReadLine(char* buf, tile_t* last){
-	tile_t* lt = last;
-	tile_t* tmp = NULL;
-	tile_t* new = NULL;
-	while(*buf){
-		if(lt == NULL){
-			lt = newTile((*buf == '@')?(1):(0));
-			buf++;
-			continue;
-		}
-		
-		tmp = lt;
-		while (tmp->left != NULL){
-			tmp = tmp->left;
-		}
-
-		new = 
-		
-		buf++;
-	}
-	printf("\n");
-	
-	return lt;
+int ReadLine(char* buf){
+	return 0;
 }
 
 int ReadFile(char* path){
 	FILE* file;
-	tile_t* last = NULL;
-	char buffer[BUFFER_SIZE];
-	for (int _ = 0; _ < BUFFER_SIZE; _++){buffer[_] = 0;}
+	//char buffer[BUFFER_SIZE];
+	//for (int _ = 0; _ < BUFFER_SIZE; _++){buffer[_] = 0;}
 	
 	file = fopen(path,"r");
 	if(file == NULL){
 		return -1;
 	}
 
-	int _ind = 0;
+	int x = 0;
+	int y = 0;
+
+	//int _ind = 0;
+	int _x = 0;
 	while (!feof(file)){
 		char c = fgetc(file);
 		if(c == '\n'){
-			last = ReadLine(buffer,last);
-			for (int _ = 0; _ < BUFFER_SIZE; _++){buffer[_] = 0;}
-			_ind = 0;
+			//for (int _ = 0; _ < BUFFER_SIZE; _++){buffer[_] = 0;}
+			//_ind = 0;
+			_x = 0;
+			y++;
 		}else{
-			buffer[_ind++] = c;
+			_x++;
+			if (_x > x){
+				x = _x;
+			}
+			
+			//buffer[_ind++] = c;
 		}
 	}
 
-	buffer[_ind - 1] = 0;
-	ReadLine(buffer,last);
-	for (int _ = 0; _ < BUFFER_SIZE; _++){buffer[_] = 0;}
-	_ind = 0;
+	fseek(file,0,SEEK_SET);
+	int arr[y][x];
+	for(int i = 0; i < y; i++){for(int j = 0; j < x; j++){arr[i][j] = 0;}}
+	
+	_x = 0;
+	int _y = 0;
+	while (!feof(file)){
+		char c = fgetc(file);
+		if(c == '\n'){
+			_x = 0;
+			_y++;
+		}else{
+			arr[_y][_x] = (c == '@')?('@'):('.');
+			_x++;
+		}
+	}
+
+	for(int i = 0; i < y; i++){
+		for(int j = 0; j < x; j++){
+			printf("%c",arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
 
 	fclose(file);
 	return 0;
